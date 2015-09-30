26bb01c212fc8515e797212d1d17542bb13fe4f9
Kumar Agarwal <kumaragarwal@Kumars-MacBook-Pro.local>
2015-09-29 18:45:28 -0700

    sign in working

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, json, request
+import logging
+from flask import Flask, render_template, json, request, redirect, session
 from flask.ext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = 'why would I tell you my secret key?'
 mysql = MySQL()
  
 # MySQL configurations
@@ -56,7 +58,44 @@ def signUp():
 
 @app.route('/showSignin')
 def showSignin():
-    return render_template('signin.html')
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('signin.html')
+    
+@app.route('/userHome')
+def userHome():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('error.html',error = 'Unauthorized Access')
+
+@app.route('/validateLogin',methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+        
+        # connect to mysql
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('sp_validateLogin',(_username,))
+        data = cursor.fetchall()
+        
+        if len(data) > 0:
+            if check_password_hash(str(data[0][3]),_password):
+                session['user'] = data[0][0]
+                return redirect('/userHome')
+            else:
+                return render_template('error.html',error = 'Wrong Email address or Password.')
+        else:
+            return render_template('error.html',error = 'Wrong Email address or Password.')
+
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    finally:
+        cursor.close()
+        con.close()
 
 if __name__ == "__main__":
     app.run(debug = True)
--- sql/initial_tables.sql
+++ sql/initial_tables.sql
@@ -2,16 +2,16 @@ CREATE DATABASE BucketList;
 
 CREATE TABLE `BucketList`.`tbl_user` (
   `user_id` BIGINT NULL AUTO_INCREMENT,
-  `user_name` VARCHAR(45) NULL,
-  `user_username` VARCHAR(45) NULL,
-  `user_password` VARCHAR(45) NULL,
+  `user_name` VARCHAR(255) NULL,
+  `user_username` VARCHAR(255) NULL,
+  `user_password` VARCHAR(255) NULL,
   PRIMARY KEY (`user_id`));
 
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_createUser`(
-    IN p_name VARCHAR(20),
-    IN p_username VARCHAR(20),
-    IN p_password VARCHAR(20)
+    IN p_name VARCHAR(255),
+    IN p_username VARCHAR(255),
+    IN p_password VARCHAR(255)
 )
 BEGIN
     if ( select exists (select 1 from tbl_user where user_username = p_username) ) THEN
@@ -39,7 +39,7 @@ DELIMITER ;
 
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_validateLogin`(
-IN p_username VARCHAR(20)
+IN p_username VARCHAR(255)
 )
 BEGIN
     select * from tbl_user where user_username = p_username;
