b5a0094106a134ec78d6628067c9be79fc57b7c9
Kumar Agarwal <kumaragarwal@Kumars-MacBook-Pro.local>
2015-09-29 18:08:03 -0700

    stored procedure for login

--- sql/initial_tables.sql
+++ sql/initial_tables.sql
@@ -36,3 +36,12 @@ BEGIN
     END IF;
 END$$
 DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_validateLogin`(
+IN p_username VARCHAR(20)
+)
+BEGIN
+    select * from tbl_user where user_username = p_username;
+END$$
+DELIMITER ;
