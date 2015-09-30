60bed18f3ffac5be6cdc39441a47e155b2fcd780
Kumar Agarwal <kumaragarwal@Kumars-MacBook-Pro.local>
2015-09-29 17:04:31 -0700

    sign up controller and view

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
     
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+    
 if __name__ == "__main__":
     app.run()
     
